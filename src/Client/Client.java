97a341f8644c9775c056e412dca74d852061d1e8
ivan-bulkin <oleg@6516363.ru>
2021-08-14 14:58:37 +0300

    доделал, чтобы пользователю нельзя было войти под именем, которое уже используется

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 8188);
+            Socket socket = new Socket("localhost", 8188);//193.168.46.140
             System.out.println("Вы успешно подключёны");
             DataInputStream in = new DataInputStream(socket.getInputStream());//это поток ввода
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());//это поток вывода
