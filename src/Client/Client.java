292e3f7d4a22521af79ae90fb781b237d3b9ba6c
ivan-bulkin <oleg@6516363.ru>
2021-08-14 11:37:43 +0300

    мой первый commit Чата

---
+++ src/Client/Client.java
@@ -0,0 +1,46 @@
+package Client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 8188);
+            System.out.println("Вы успешно подключёны");
+            DataInputStream in = new DataInputStream(socket.getInputStream());//это поток ввода
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());//это поток вывода
+            String response = in.readUTF();//принимаем и читаем ответ от Сервера
+            System.out.println(response);
+            response = in.readUTF();
+            System.out.print(response);//Здесь печатаем приглашение о вводе имени(приходит от Сервера)
+            Scanner scanner = new Scanner(System.in);//Это нужно, чтобы можно было вводить значения с клавиатуры
+            out.writeUTF(scanner.nextLine());//Ожидаем ввод имени
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String response;
+                    while (true) {//Бесконечно ожидаем сообщения от сервера
+                        try {
+                            response = in.readUTF();//принимаем и читаем ответ от Сервера
+                            System.out.println(response);//
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+            while (true) {
+//                System.out.print("Введите Ваше сообщение: ");
+                String consoleText = scanner.nextLine();//Ждём сообщение от пользователя
+                out.writeUTF(consoleText);//Отправялем сообщение на сервер
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,67 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    public static void main(String[] args) {
+        {
+            ArrayList<User> users = new ArrayList<>();//Коллекция с Клиентами
+            try {
+                ServerSocket serverSocket = new ServerSocket(8188);//Создаём серверный сокет
+                System.out.println("Сервер запущен");
+                while (true) {//Бесконечный цикл для ожидания подключения Клиентов
+                    Socket socket = serverSocket.accept();//Ожидаем подключения клиента
+                    System.out.println("Клиент подключился");
+                    User currentUser = new User(socket);//создаём класс Пользователя
+                    users.add(currentUser);
+                    DataInputStream in = new DataInputStream(currentUser.getSocket().getInputStream());//это поток ввода
+                    DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream());//это поток вывода
+//это третий способ многопоточности
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                out.writeUTF("Добро пожаловать на Сервер");
+                                out.writeUTF("Введите Ваше имя: ");
+                                String userName = in.readUTF();//Ожидаем имя от Клиента
+                                currentUser.setUserName(userName);
+                                for (User user : users) {
+                                    DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                    out.writeUTF(currentUser.getUserName() + " теперь в чате.");
+                                }
+                                while (true) {
+                                    String request = in.readUTF();//читаем сообщения от Клиента
+                                    System.out.println(currentUser.getUserName() + ": " + request);//Логирование действий пользователя на Сервере
+                                    for (User user : users) {
+                                        if (users.indexOf(currentUser) == users.indexOf(user)) continue;
+                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                        out.writeUTF(currentUser.getUserName() + ": " + request);
+                                    }
+                                }
+                            } catch (IOException e) {
+                                users.remove(currentUser);//удаляем Клиента из коллекции Пользователей
+                                for (User user : users) {
+                                    try {
+                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                        out.writeUTF(currentUser.getUserName() + " теперь НЕ в чате.");
+                                    } catch (IOException ex) {
+                                        ex.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    );
+                    thread.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
